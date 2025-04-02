b2518740bb6d338239d5cbd5b39af81c3e374afc
caoye04 <caoye541@gmail.com>
2025-04-02 21:18:41 +0800

    last version

--- code/benchmark.sh
+++ code/benchmark.sh
@@ -13,7 +13,7 @@ export OMP_NUM_THREADS=$3
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 srun -N 1 -n $2 --cpus-per-task=$3 ./$1 27 256 256 256 100 ${DATAPATH}/stencil_data_256x256x256
-#salloc -N $2 --ntasks-per-node $3 mpirun $1 7 384 384 384 100 ${DATAPATH}/stencil_data_384x384x384
+#salloc -N $2 --ntasks-per-node $3 mpirun $1 7  100 ${DATAPATH}/stencil_data_384x384x384
 #salloc -N $2 --ntasks-per-node $3 mpirun $1 7 512 512 512 100 ${DATAPATH}/stencil_data_512x512x512
 #salloc -N $2 --ntasks-per-node $3 mpirun $1 27 256 256 256 100 ${DATAPATH}/stencil_data_256x256x256
 #salloc -N $2 --ntasks-per-node $3 mpirun $1 27 384 384 384 100 ${DATAPATH}/stencil_data_384x384x384
